2196bdc8d19eab557e41c33b21885e48808c295a
Davide Bonsangue <davidebonsangue@gmail.com>
2021-10-28 15:42:33 +0200

    Added AddFixedPriceItem

---
+++ src/Trading/AddFixedPriceItemRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrainStorm\Ebay\Trading;
+
+use BrainStorm\Ebay\EbayRequest;
+use BrainStorm\Ebay\Request\WSAPI;
+
+class AddFixedPriceItemRequest
+{
+	protected $request;
+	protected $ebayClient;
+
+    public function __construct(EbayRequest $ebayClient)
+    {
+		$this->ebayClient = $ebayClient;
+        $this->request = new WSAPI($ebayClient);
+    }
+	
+    public function doRequest($refresh_token, string $body, bool $cached = false, int $expire = 86400)
+    {
+		$response = $this->request->POST($refresh_token, 'AddFixedPriceItem', $body);
+		$result = array();
+		$i = 0;
+		$to_obj = simplexml_load_string($response->getBody()->getContents());
+		return $to_obj;
+	}
+}
